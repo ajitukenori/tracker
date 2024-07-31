341e02cf0ce67745e82cd3b5cddcad16d982a613
ajitukenori <nakamura230456@eggnet.ac.jp>
2024-07-31 18:19:12 +0900

    [feat]フォントデザイン修正

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,4 @@ import "./static/css/ress.min.css";
 import '@fontsource/fredericka-the-great';
 import '@fontsource-variable/quicksand';
 import '@fontsource/kiwi-maru';
+import '@fontsource/yusei-magic';
