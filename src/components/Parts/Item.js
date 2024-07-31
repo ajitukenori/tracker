006146d8fbcb70e0c37b0201311892c202251f3a
ajitukenori <nakamura230456@eggnet.ac.jp>
2024-07-31 14:05:35 +0900

    [feat]タップ範囲修正

--- src/components/Parts/Item.js
+++ src/components/Parts/Item.js
@@ -13,7 +13,7 @@ const ItemParts = ({ tracker, taskList }) => {
 
     return (
         <>
-            <div className="task">
+            <div className="task pc">
                 <div className="task-info">
                     <div className={tracker.count < 7 ? 'task-check-min' : 'task-check' } >
                         {[...Array(tracker.count)].map((_, index) => (
@@ -32,6 +32,25 @@ const ItemParts = ({ tracker, taskList }) => {
                     </div>
                 </div>
             </div>
+            <button className="task sp"  onClick={clearTask}>
+                <div className="task-info">
+                    <div className={tracker.count < 7 ? 'task-check-min' : 'task-check' } >
+                        {[...Array(tracker.count)].map((_, index) => (
+                            <div className={tracker.count < 7 ?'clear-start' : 'task-clear-check'}>
+                                <img src={`/image/color-icon/${tracker._color.image}`} alt="" className='task-clear-img' />
+                            </div>
+                        ))}
+                        <button className={`${tracker.is_checked ? "check-true" : "task-clear"} ${tracker.count < 7 ?'clear-start' : 'task-clear-check'}`}>
+                            <img src="/image/color-icon/color-01.svg" alt="" className='task-clear-img' />
+                        </button>
+                    </div>
+                    <div className="task-name">
+                        <div className="task-name-info">
+                            <span>{tracker.name}</span>
+                        </div>
+                    </div>
+                </div>
+            </button>
         </>
     )
 
